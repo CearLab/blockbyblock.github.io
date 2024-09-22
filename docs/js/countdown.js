61cb4349eaa77729d39931176c44dbeb46f95cf7
Federico Oliva <federico.oliva.it@gmail.com>
2024-09-22 14:26:16 +0300

    dev: abstract, style

--- docs/js/countdown.js
+++ docs/js/countdown.js
@@ -1,20 +1,19 @@
 // Javascript
-$(function() {
+$(function () {
   var currentDate = new Date();
-//  $('.countdown').countdown("yyyy/MM/dd/hh:mm:ss", function(event)
-//	Delete line 6
-  $('.time').countdown(25 * 24 * 60 * 60 * 1000 + currentDate.valueOf(), function(event)
-//	until here
-	{
+  var targetDate = new Date("2025/05/18 23:59:59"); // Set your target date here
+  var timeDifference = targetDate - currentDate; // Calculate the time difference in milliseconds
+
+  $('.time').countdown(timeDifference + currentDate.valueOf(), function (event) {
     $this = $(this);
-    switch(event.type) {
+    switch (event.type) {
       case "seconds":
       case "minutes":
       case "hours":
       case "days":
       case "weeks":
       case "daysLeft":
-        $this.find('span.'+event.type).html(event.value);
+        $this.find('span.' + event.type).html(event.value);
         break;
       case "finished":
         $this.fadeTo('slow', .5);
