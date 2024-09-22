68f29d9e038672e6343a8a3e3fc22b3a6dd3d1c5
Federico Oliva <federico.oliva.it@gmail.com>
2024-09-22 15:22:19 +0300

    fix: send email

--- docs/contact.php
+++ docs/contact.php
@@ -1,36 +1,25 @@
 <?php
-// Change the 3 variables below
-$yourName = 'Federico Oliva';
-$yourEmail = 'federico.oliva.it@gmail.com';
-$subject = 'New subscription - BlockByBlock Worskhop';
+	// Change the 3 variables below
+	$yourName = 'Federico Oliva';
+	$yourEmail = 'federico.oliva.it@gmail.com';
+	$subject = 'New subscription - BlockByBlock Workshop';
 
-$ch = $_POST['choice'];
-$email = $_POST['email'];
+	$ch = $_POST['choice'];
+	$email = $_POST['email'];
 
-if($ch=="contact")
-{
+	if($ch == "contact") {
+		$message = "From: " . htmlspecialchars($_POST['name']) . "\n Email: " . htmlspecialchars($email) . "\r\n";
+		$message .= "Hello there! Thanks for subscribing to the Workshop mailing list! Stay tuned for more information!";
+		$subject = "New subscription - BlockByBlock Workshop";
+	} else {
+		$message = "\n Email: " . htmlspecialchars($email);
+	}
 
-$message = "From: ".$_POST[$yourName]."\n Email: $email \r\n";
-$message = $message.$_POST['Hello there! Thanks for subscribing to the Workshop mailing list! Stay tuned for more information!'];
-$subject = "New subscription - BlockByBlock Workshop";
+	$mailit = mail($yourEmail, $subject, $message, "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n");
 
-}
-else
-{
-	$message = "\n Email: $email ";
-}
-
-
-$mailit = mail($yourEmail, $subject, $message, "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n");
-
-
-
-		if ($mailit ) {
+	if ($mailit) {
 		echo "success";
-		}
-		else {
+	} else {
 		echo "error";
-		}
-
-
+	}
 ?>
