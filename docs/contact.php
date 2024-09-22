2124399a1d037c88772be16e6765b701f632e1be
Federico Oliva <federico.oliva.it@gmail.com>
2024-09-22 16:14:24 +0300

    chore: change php

--- docs/contact.php
+++ docs/contact.php
@@ -4,22 +4,37 @@
 	$yourEmail = 'federico.oliva.it@gmail.com';
 	$subject = 'New subscription - BlockByBlock Workshop';
 
-	$ch = $_POST['choice'];
-	$email = $_POST['email'];
+	if (isset($_POST['choice']) && isset($_POST['email'])) {
+		$ch = $_POST['choice'];
+		$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-	if($ch == "contact") {
-		$message = "From: " . htmlspecialchars($_POST['name']) . "\n Email: " . htmlspecialchars($email) . "\r\n";
-		$message .= "Hello there! Thanks for subscribing to the Workshop mailing list! Stay tuned for more information!";
-		$subject = "New subscription - BlockByBlock Workshop";
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			echo "Invalid email address";
+			exit();
+		}
+
+		if ($ch == "contact" && isset($_POST['name'])) {
+			$name = htmlspecialchars($_POST['name']);
+			$message = "From: " . $name . "\n Email: " . $email . "\r\n";
+			$message .= "Hello there! Thanks for subscribing to the Workshop mailing list! Stay tuned for more information!";
+			$subject = "New subscription - BlockByBlock Workshop";
+		} else {
+			$message = "\n Email: " . $email;
+		}
+
+		$headers = "From: $email\r\n";
+		$headers .= "Reply-To: $email\r\n";
+		$headers .= "Return-Path: $email\r\n";
+
+		$mailit = mail($yourEmail, $subject, $message, $headers);
+
+		if ($mailit) {
+			echo "success";
+		} else {
+			error_log("Mail failed to send to $yourEmail", 0);
+			echo "error";
+		}
 	} else {
-		$message = "\n Email: " . htmlspecialchars($email);
-	}
-
-	$mailit = mail($yourEmail, $subject, $message, "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n");
-
-	if ($mailit) {
-		echo "success";
-	} else {
-		echo "error";
+		echo "Missing required fields";
 	}
 ?>
